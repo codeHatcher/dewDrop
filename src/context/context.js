a23bd2b11f15bd04c51a42fbeec54d24f5a38b72
Avishaan <avishaan@codeHatcher.com>
2014-05-14 12:54:06 -0700

    refactor (ugh) again for readability

--- src/context/context.js
+++ src/context/context.js
@@ -1,26 +1,43 @@
 
 var dewDrop = {
+  template: {}, //keep the template here
+  init: function(){
+    this.createContexMenu();
+  },
   createContexMenu: function(){
     //send message to background page for menu creation.
     chrome.extension.sendMessage({"event": "createMenu"}, function(response){
       console.log("creating menu " + response);
     });
   },
-  template: function(cb){
+  getTemplate: function(cb){
     chrome.extension.sendMessage({"event": "getTemplateHTML"}, function(template){
       console.log("getting template from background page");
       //once the template has loaded go ahead and run the callback with the template as the first arg
+
+      $('body').append(template);
       return cb(template);
     });
+  },
+  modal: function(){
+    //setup a listener on a hidden element
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+      if (request.event === "menuClicked"){
+        //if we have a click from the context menu
+        //go ahead and setup the dialog
+        $('#dewDrop').avgrund({
+          width: 380,
+          height: 240,
+          template: "Test Template"
+        });
+        //then go ahead and trigger the dialog that we have setup
+        $('#dewDrop').trigger("click");
+      }
+    });
+    //setup modal
   }
 };
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  if (request.event === "menuClicked"){
-    //if we have a click from the context menu, go ahead and show our modal dialog
-    $('#dewDrop').trigger("click");
-  }
-});
 
 chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
@@ -31,10 +48,9 @@ chrome.extension.sendMessage({}, function(response) {
     // This part of the script triggers when page is done loading
     console.log("Hello. This message was sent from scripts/inject.js");
     // ----------------------------------------------------------
-    dewDrop.createContexMenu();
-    dewDrop.template(function(template){
-      $('body').append(template);
-      $('#dewDrop').avgrund();
+    dewDrop.init();
+    dewDrop.getTemplate(function(template){
+      dewDrop.modal();
     });
 
   }
